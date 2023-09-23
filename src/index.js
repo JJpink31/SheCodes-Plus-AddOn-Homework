868b47a2edd715d6823586a9d819c56abd8184cb
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2023-09-23 13:33:46 -0500

    Omitted the unnecessary function and put all cities in lowercase

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-function showSelectedCountry(event){
-    alert(event.target.value);
-}
  function showSelectedCountry(event){
-    if (event.target.value.length >= 0){
-      if (event.target.value === "Belgrade"){
-        let belgradeTime=moment.tz("Europe/Belgrade").format("dddd, MMMMM D, YYYY h:m A");
+      if (event.target.value === "belgrade"){
+        let belgradeTime = moment.tz("Europe/Belgrade").format("dddd, MMMMM D, YYYY h:m A");
         alert (`It is currently ${belgradeTime} in Belgrade,Serbia`);
     }
 
-     if (event.target.value === "Dubai"){
-         let dubaiTime=moment.tz("Asia/Dubai").format("dddd, MMMMM D, YYYY h:m A");
+     if (event.target.value === "dubai"){
+         let dubaiTime = moment.tz("Asia/Dubai").format("dddd, MMMMM D, YYYY h:m A");
         alert (`It is currently ${dubaiTime} in Dubai, United Arab Emirates`);
     }
     
-     if (event.target.value === "Cairo"){
-              let ciaroTime=moment.tz("Africa/Cairo").format("dddd, MMMMM D, YYYY h:m A");
+     if (event.target.value === "cairo"){
+              let ciaroTime = moment.tz("Africa/Cairo").format("dddd, MMMMM D, YYYY h:m A");
         alert (`It is currently ${ciaroTime} in Ciaro, Egypt`);  
     }
  }
-}
+
 
 let countrySelect = document.querySelector("#country");
 countrySelect.addEventListener("change",showSelectedCountry);
