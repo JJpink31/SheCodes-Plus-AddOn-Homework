17620472ad43f61b7efe96b931a6908d6e142039
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2023-09-23 11:33:19 -0500

    Added moment() timezone to function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 function showSelectedCountry(event){
+    alert(event.target.value);
+}
+ function showSelectedCountry(event){
+    if (event.target.value.length >= 0){
+      if (event.target.value === "Belgrade"){
+        let belgradeTime=moment.tz("Europe/Belgrade").format("dddd, MMMMM D, YYYY h:m A");
+        alert (`It is currently ${belgradeTime} in Belgrade,Serbia`);
+    }
+
+     if (event.target.value === "Dubai"){
+         let dubaiTime=moment.tz("Asia/Dubai").format("dddd, MMMMM D, YYYY h:m A");
+        alert (`It is currently ${dubaiTime} in Dubai, United Arab Emirates`);
+    }
     
-    if (event.target.value === "Belgrade"){
-        alert ("Belgrade");
+     if (event.target.value === "Cairo"){
+              let ciaroTime=moment.tz("Africa/Cairo").format("dddd, MMMMM D, YYYY h:m A");
+        alert (`It is currently ${ciaroTime} in Ciaro, Egypt`);  
     }
-     if (event.target.value === "New Delhi"){
-        alert ("New Delhi");
-    }
-     if (event.target.value === "Montego Bay"){
-        alert ("Montego Bay");
-    }
+ }
 }
 
 let countrySelect = document.querySelector("#country");
